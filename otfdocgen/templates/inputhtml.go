7200c8a4d68ea3acdec3035aaeb23737d49a236c
Kevin Scardina <kevin.scardina@davita.com>
2018-11-25 10:38:03 -0700

    update html for srv

--- otfdocgen/templates/inputhtml.go
+++ otfdocgen/templates/inputhtml.go
@@ -3,14 +3,21 @@ package templates
 const InputHtml = `
 <html>
 	<head>
-		<title>Upload file</title>
+		<title>OTFDocGen Web Interface</title>
 	</head>
 	<body>
+
+<pre>
+This is a simple Web Interface to the otfdocgen commandline tool.
+
+Choose the OTF file you would like to create a "doc" for and click on the type of doc to create.
+</pre>
+<hr />
 		<form enctype="multipart/form-data" action="http://localhost:%s/upload" method="post">
 			<input type="file" name="uploadfile" />
-			<input type="hidden" name="token" value="{{.}}"/>
-			<input type="submit" value="swift" name="submit" />
+			<input type="submit" value="md" name="submit" />
 			<input type="submit" value="html" name="submit" />
+			<input type="submit" value="swift" name="submit" />
 		</form>
 	</body>
 </html>
